318f62d783c07f6aaf1e5f913dee3ba92bc25f13
Tainara <tainara_rafaely@hotmail.com>
2025-01-21 23:44:49 -0300

    Implementação Stories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import NavBar from "./NavBar";
+import Stories from "./Stories";
 
 export default function App() {
     return (
         <>
             <NavBar />
+            <div class="corpo">
+                <div class="esquerda">
+                    <Stories/>
+                </div>
+            </div>
         </>
     )
 }
---
+++ src/Stories.js
@@ -0,0 +1,32 @@
+export default function Stories() {
+    const stories = [
+        { nome: "9gag", imagem: "assets/img/9gag.svg" },
+        { nome: "meowed", imagem: "assets/img/meowed.svg" },
+        { nome: "barked", imagem: "assets/img/barked.svg" },
+        { nome: "nathanwpylestrangeplanet", imagem: "assets/img/nathanwpylestrangeplanet.svg" },
+        { nome: "wawawicomics", imagem: "assets/img/wawawicomics.svg" },
+        { nome: "respondeai", imagem: "assets/img/respondeai.svg" },
+        { nome: "filomoderna", imagem: "assets/img/filomoderna.svg" },
+        { nome: "memeriagourmet", imagem: "assets/img/memeriagourmet.svg" }
+    ];
+
+    return (
+        <div class="stories">
+            {stories.map((story, index) => <Story nome={story.nome} imagem={story.imagem} key={index}/>)}
+        </div>
+    )
+}
+
+function Story({nome, imagem}) {
+    return (
+        <div class="story">
+            <div class="imagem">
+                <img src={imagem} alt={nome} />
+            </div>
+            <div class="usuario">
+                {nome}
+            </div>
+        </div>
+    )
+
+}
