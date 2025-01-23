24316f2c6ca9e5ead416877925a1ee2051121bab
Tainara <tainara_rafaely@hotmail.com>
2025-01-22 22:32:50 -0300

    Alteração imagem de perfil

--- src/User.js
+++ src/User.js
@@ -1,20 +1,24 @@
 import React from "react";
 
 export default function User() {
-    const avatarInicial = "assets/img/catanacomics.svg";
+    const avatarInicial = "https://i.pinimg.com/736x/ae/ae/d2/aeaed2f94d13e2faf3b72b010e681684.jpg";
 
     const [nome, setNome] = React.useState("");
-    
+    const [avatar, setAvatar] = React.useState(avatarInicial);
 
     function inserirNome() {
         const nomeInserido = prompt("Qual é o seu nome?");
         setNome(nomeInserido);
     }
 
+    function mudarAvatar() {
+        const linkInserido = prompt("Digite o link da sua imagem");
+        setAvatar(linkInserido);
+    }
 
     return (
         <div class="usuario">
-          <img src={avatarInicial} alt="imagem de perfil"/>
+          <img onClick={mudarAvatar} src={!avatar ? avatarInicial : avatar} alt="imagem de perfil"/>
           <div class="texto">
             <span>
               <strong>{!nome ? "Tainara" : nome}</strong>
